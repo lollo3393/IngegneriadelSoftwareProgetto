b3fef061b09bf2f734c3323f18de23923e22d52a
lollo3393 <pndlnz03l11g791q@studenti.unical.it>
2025-06-04 16:36:02 +0200

    Interfaccia Libro creata

---
+++ GestioneLibreriaPersonale/src/main/java/Libreria/Modello/Libro.java
@@ -0,0 +1,23 @@
+package Libreria.Modello;
+//interfaccia libro iniziale potrebbe ancora variare
+public interface Libro {
+    String getTitolo();
+    void setTitolo(String autore);
+
+    String getISBN();
+    String setISBN(String ISBN);
+    String getAutore();
+    void setAutore(String autore);
+    String getGenere();
+    void setGenere(String genere);
+    int getValutazione();
+    void setValutazione(int valutazione);
+
+    String getStatoDiLettura();
+    void setStatoDiLettura(String statoDiLettura);
+    String getRecensione();
+    void setRecensione(String recensione);
+
+
+
+}
